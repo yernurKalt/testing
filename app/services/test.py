5540f1eb0d26cff278d14c699fc1af46ef0fcd3e
Yernur <kaltazik009@gmail.com>
2026-03-31 17:29:03 +0500

    added reservation system

--- app/services/test.py
+++ app/services/test.py
@@ -1,33 +1,8 @@
-"""
-Demonstrates how to use the asyncio compatible scheduler to schedule a job that executes on 3
-second intervals.
-"""
-
 import asyncio
-import os
-from datetime import datetime
-
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-
-def tick():
-    print(f"Tick! The time is: {datetime.now()}")
-
 
 async def main():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(tick, "interval", seconds=3)
-    scheduler.start()
-    print("Press Ctrl+{} to exit".format("Break" if os.name == "nt" else "C"))
-    try:
-        await asyncio.Event().wait()
-    finally:
-        scheduler.shutdown(wait=False)
+    print('Hello ...')
+    await asyncio.sleep(1)
+    print('... World!')
 
-
-if __name__ == "__main__":
-    # Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        pass
+asyncio.run(main())
